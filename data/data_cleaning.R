eabc28f4fa8b399a405d1f4c3e26213dbf3e06f0
fbazzi19 <fbazzi19@gmail.com>
2025-10-01 15:57:39 -0400

    data cleaning and formatting as csv

---
+++ data/data_cleaning.R
@@ -0,0 +1,32 @@
+library(readxl)
+library(dplyr)
+#data load
+mutations <- readRDS("tcga_maf_filter_binary.Rds")
+metadata <- read_xlsx("TCGA-CDR-SupplementalTableS1.xlsx")
+subtype_data <- read.table("TCGASubtype.20170308.tsv", sep = "\t", header = TRUE)
+
+#intersection of samples with metadata and mutation
+ids_w_muts <- unlist(lapply(mutations$Tumor_Sample_Barcode, function(x) substr(x, 1, 12)))
+ids_in_both <- intersect(metadata$bcr_patient_barcode, ids_w_muts)
+
+metadata <-  metadata[metadata$bcr_patient_barcode %in% ids_in_both,]
+mutations$patient_id <- ids_w_muts
+mutations<- mutations[mutations$patient_id %in% ids_in_both,]
+mutations <- mutations %>% distinct(patient_id, .keep_all = TRUE)
+
+#keep only COAD samples
+mutations$type <- metadata[unlist(mapply(function(X) {which(metadata[,"bcr_patient_barcode"]==X)}, X=mutations$patient_id)), "type"]
+mutations_coad <- mutations[which(mutations$type=="COAD"),]
+metadata_coad <- metadata[which(metadata$type=="COAD"),]
+subtype_coad <- subtype_data[which(subtype_data$cancer.type=="COAD"),]
+
+#intersect with subtype
+ids_in_all <-intersect(metadata$bcr_patient_barcode, subtype_coad$pan.samplesID)
+metadata_coad <-  metadata[metadata$bcr_patient_barcode %in% ids_in_all,]
+mutations_coad<- mutations[mutations$patient_id %in% ids_in_all,]
+subtype_coad <- subtype_coad[subtype_coad$pan.samplesID %in% ids_in_all,]
+
+#download data
+write.csv(metadata_coad, "cleaned_metadata.csv")
+write.csv(mutations_coad, "cleaned_mut_data.csv")
+write.csv(subtype_coad, "cleaned_subtype_data.csv")
