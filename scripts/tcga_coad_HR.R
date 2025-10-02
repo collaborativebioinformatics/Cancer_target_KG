5658a3dd774beeadfc0abcd1a5e79be25fbed080
Arshi Arora <arshiarora@Arshis-MacBook-Pro.local>
2025-10-02 11:32:13 -0400

    add pway information

--- scripts/tcga_coad_HR.R
+++ scripts/tcga_coad_HR.R
@@ -7,27 +7,62 @@ coad_maf <- read.csv(file="data/coad_mut_data.csv")
 coad_clin <- read.csv(file="data/coad_metadata.csv") %>%
   dplyr::select(-`...1`)
 
-coad_clin <- coad_clin %>%
-  mutate(age_bin = case_when(age_at_initial_pathologic_diagnosis <= 50 ~ 1, 
-                             age_at_initial_pathologic_diagnosis > 50 ~ 0, 
-                             TRUE ~ NA))
 
-write.csv(coad_clin, file="data/coad_metadata.csv", row.names = FALSE)
+# filter genes 
+sum_gg <- coad_maf %>% 
+  dplyr::select(-c(Tumor_Sample_Barcode, patient_id)) %>%
+  apply(., 2, sum)
+
+# do a filter of <2%
+
+gg_cut_off <- round(length(unique(coad_maf$Tumor_Sample_Barcode)) * 0.02)
+
+sum_gg_filter <- sum_gg[which(sum_gg > gg_cut_off)]
+
+coad_maf_filter <- coad_maf %>% dplyr::select(patient_id, all_of(names(sum_gg_filter)))
+
+# cox model 
+
+for(i in 1:length(sum_gg_filter)){
+  
+  
+  
+  
+  
+}
 
 
-# Read in colorectal cancer data from TCGA
+#on the same genes add pathway information 
 
-# Merge mutation data with meta-data
+library(msigdbr)
 
-# Create age groups (<= 50 and > 50 years old)
+human_canoncial_pways <- msigdbr(species = "Homo sapiens", category = "C2")
 
-# Define models for young vs. old age groups
+human_canoncial_pways_filter <- human_canoncial_pways %>%
+  dplyr::filter(gs_subcat %in% c("CP:BIOCARTA", "CP:KEGG", "CP:REACTOME"))
 
-# Perform survival analysis using the survival package
+#filter pathways with more than 200 genes 
 
-## Data diagnostics: test proportional hazards assumption
+human_canoncial_pways_filter <- human_canoncial_pways_filter %>%
+  group_by(gs_name) %>%
+  mutate(n_genes = n()) %>%
+  ungroup() %>%
+  dplyr::filter(n_genes <= 200)
 
-## Cox model where outcome is overall survival, exposure is gene mutation status, and
-## there is an interaction term for age (young vs. old)
 
-## Plot survival curves (Kaplan-Meier plots) with separate lines for young vs. old age groups
+human_canoncial_pways_filter_wide <- human_canoncial_pways_filter %>%
+  dplyr::select(gs_name, gene_symbol) %>%
+  distinct() %>%
+  pivot_wider(id_cols = gene_symbol, names_from = gs_name, 
+              values_from = gs_name, values_fill = 0, values_fn = function(x) 1)
+
+#filter on genes that are less prevalent 
+
+human_canoncial_pways_filter_wide <- human_canoncial_pways_filter_wide %>%
+  dplyr::filter(gene_symbol %in% names(sum_gg_filter))
+
+
+write.csv(human_canoncial_pways_filter_wide, file="data/human_canonical_pways.csv", row.names = FALSE)
+
+
+
