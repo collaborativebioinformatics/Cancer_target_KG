88fe9a04bf8428df85c73c4bb6a0bbd713f03ca0
fbazzi19 <fbazzi19@gmail.com>
2025-10-02 10:53:16 -0400

    further cleaning of metadata

--- data/data_cleaning.R
+++ data/data_cleaning.R
@@ -6,7 +6,6 @@ metadata <- read_xlsx("TCGA-CDR-SupplementalTableS1.xlsx")
 subtype_data <- read.table("TCGASubtype.20170308.tsv", sep = "\t", header = TRUE)
 
 #intersection of samples with metadata and mutation
-#NO LONGER DOING
 ids_w_muts <- unlist(lapply(mutations$Tumor_Sample_Barcode, function(x) substr(x, 1, 12)))
 ids_in_both <- intersect(metadata$bcr_patient_barcode, ids_w_muts)
 
@@ -21,13 +20,18 @@ mutations_coad <- mutations[which(mutations$type=="COAD"),]
 metadata_coad <- metadata[which(metadata$type=="COAD"),]
 subtype_coad <- subtype_data[which(subtype_data$cancer.type=="COAD"),]
 
-#intersect with subtype
-#ids_in_all <-intersect(metadata$bcr_patient_barcode, subtype_coad$pan.samplesID)
-#metadata_coad <-  metadata[metadata$bcr_patient_barcode %in% ids_in_all,]
-#mutations_coad<- mutations[mutations$patient_id %in% ids_in_all,]
-#subtype_coad <- subtype_coad[subtype_coad$pan.samplesID %in% ids_in_all,]
 
 mutations_coad <- mutations_coad[, -which(names(mutations_coad) == "type")]
+
+#add col to meta indicating binary age
+metadata_coad <- metadata_coad %>% mutate(older_than_50 = ifelse(age_at_initial_pathologic_diagnosis > 50, 1, 0))
+
+#remove irrelevant cols
+metadata_coad <- metadata_coad[, -which(names(metadata_coad) %in% 
+                                c("...1", "clinical_stage", "histological_grade",
+                                  "menopause_status", "Redaction", "margin_status",
+                                  "residual_tumor","new_tumor_event_site","new_tumor_event_site_other",
+                                  "cause_of_death", "new_tumor_event_type"))]
 #download data
 write.csv(metadata_coad, "coad_metadata.csv", quote = FALSE, row.names = FALSE)
 write.csv(mutations_coad, "coad_mut_data.csv", quote = FALSE, row.names = FALSE)
